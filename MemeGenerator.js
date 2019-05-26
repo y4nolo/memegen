aff1d608b994c64f49b5d2fb26eb86432bb45463
Y4nolo <yanyee.chan@gmail.com>
2019-05-26 14:32:15 +0100

    added header

---
+++ MemeGenerator.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      ramdonImg: "http://i.imgflip.com/1bij.jpg"
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes").then(response =>
+      response.json().then(response => {
+        const { memes } = response.data;
+        console.log(memes[0]);
+        this.setState({ allMemeImgs: memes });
+      })
+    );
+  }
+
+  render() {
+    return <h1>MEME GENERATOR SECTION </h1>;
+  }
+}
+
+export default MemeGenerator;
